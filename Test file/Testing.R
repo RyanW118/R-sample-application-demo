01bad27e2bc3780c09301e82e455d8eff2b27f4a
RyanW <wenfung118@gmail.com>
2026-01-29 02:27:29 +0800

    Testing R file modified

--- Test file/Testing.R
+++ Test file/Testing.R
@@ -1,4 +1,25 @@
 library(shiny)
 
-runExample("01_hello")
+#runExample("01_hello")
 
+# 1. User Interface (UI) - This defines how the app LOOKS
+ui <- fluidPage(
+  #titlePanel("My first Hello World in Shiny"),
+  
+  mainPanel(
+    # This is the placeholder where the server will send the text
+    textOutput("greeting")
+  )
+)
+
+# 2. Server Logic - This defines what the app DOES
+server <- function(input, output) {
+  
+  # This links to the 'greeting' placeholder in the UI
+  output$greeting <- renderText({
+    "Hello World"
+  })
+}
+
+# 3. Run the Application
+shinyApp(ui = ui, server = server)
